53cf228ba1084465d74bef13d5d125b8f1ac38e9
ZaharDIO <carloshenriquegyn0204@gmail.com>
2025-05-12 20:42:49 -0300

    Mapas

---
+++ QBCoreFramework_E6CD5B.base/resources/[mapas]/ForestsOfSA_S/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- This mod was made for free by JRod and Larcius.
+-- For more information check out https://www.gta5-mods.com/maps/forests-of-san-andreas-revised
+
+fx_version('cerulean')
+game('gta5')
+
+author('JRod and Larcius')
+description('Forests of San Andreas (South): Revised')
+version('5.3')
+
+this_is_a_map('yes')
+data_file('DLC_ITYP_REQUEST')('stream/forests_s_slod/forests_s_slod.ytyp')
