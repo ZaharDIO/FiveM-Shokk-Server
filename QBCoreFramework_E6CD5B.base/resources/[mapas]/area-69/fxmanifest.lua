7cf367f6060f45d2f2c8dc0e5bae2ad77fd68607
ZaharDIO <carloshenriquegyn0204@gmail.com>
2025-05-12 20:50:08 -0300

    Mapas

---
+++ QBCoreFramework_E6CD5B.base/resources/[mapas]/area-69/fxmanifest.lua
@@ -0,0 +1,50 @@
+fx_version 'adamant'
+games {'gta5'}
+this_is_a_map 'yes'
+
+
+author 'CrazyModder127'
+description 'Adds Area 69 Underground base in Fort Zancudo'
+version '1.0 [BETA]'
+
+
+
+files {
+	'audio/a69bunker_sounds.dat54',
+	'audio/a69carrier_sounds.dat54',
+    'audio/a69sewers_sounds.dat54',
+    'audio/a69storage_sounds.dat54',
+    'audio/a69vaultroom_sounds.dat54',
+    'audio/a69carrier_game.dat149',
+    'audio/a69carrier_game.dat150',
+    'audio/a69bunker_game.dat151',
+    'audio/a69sewers_game.dat151',
+    'audio/a69storage_game.dat151',
+    'audio/a69vaultroom_game.dat151',
+    'audio/a69carrier_game.dat150.nametable',
+    'audio/a69carrier_sounds.dat54.nametable',
+    'audio/a69bunker_game.dat151.rel',
+    'audio/a69bunker_sounds.dat54.rel',
+    'audio/a69carrier_game.dat149.rel',
+    'audio/a69carrier_game.dat151.rel',
+    'audio/a69carrier_sounds.dat54.rel',
+    'audio/a69sewers_game.dat151.rel',
+    'audio/a69sewers_sounds.dat54.rel',
+    'audio/a69storage_game.dat151.rel',
+    'audio/a69storage_sounds.dat54.rel',
+    'audio/a69vaultroom_game.dat151.rel',
+    'audio/a69vaultroom_sounds.dat54.rel',
+}
+
+
+
+data_file 'AUDIO_GAMEDATA' 'audio/a69bunker_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69bunker_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/a69sewers_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69sewers_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/a69vaultroom_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69vaultroom_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/a69storage_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69storage_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/a69carrier_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69carrier_sounds.dat'
