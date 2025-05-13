a7a0cf1ad2b60ee9e385f7492051cd4a1fe86752
ZaharDIO <carloshenriquegyn0204@gmail.com>
2025-05-12 23:22:49 -0300

    Carros

---
+++ QBCoreFramework_E6CD5B.base/resources/[carros]/[catalogados]/rrgt500off/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "cerulean"
+game "gta5"
+
+files {
+    "data/vehiclelayouts.meta",
+    "data/vehicles.meta",
+    "data/carvariations.meta",
+    "data/carcols.meta",
+    "data/handling.meta",
+    "data/dlctext.meta",
+}
+
+data_file "VEHICLE_LAYOUTS_FILE"   "data/vehiclelayouts.meta"
+data_file "HANDLING_FILE"          "data/handling.meta"
+data_file "VEHICLE_METADATA_FILE"  "data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+data_file "DLC_TEXT_FILE"          "data/dlctext.meta"
+ -- you need this for vMenu
